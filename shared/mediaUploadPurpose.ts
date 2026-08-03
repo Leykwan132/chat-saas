7bf002cf9701d74b64487895b2d50105fe52d413
LEY KWAN CHOO <56620672+Leykwan132@users.noreply.github.com>
2026-08-04 00:10:06 +0800

    Use erasable media purpose values

--- shared/mediaUploadPurpose.ts
+++ shared/mediaUploadPurpose.ts
@@ -1,4 +1,7 @@
-export enum MediaUploadPurpose {
-  KnowledgeBase = 'knowledgeBase',
-  WorkflowSendMedia = 'workflowSendMedia',
-}
+export const MediaUploadPurpose = {
+  KnowledgeBase: 'knowledgeBase',
+  WorkflowSendMedia: 'workflowSendMedia',
+} as const;
+
+export type MediaUploadPurpose =
+  (typeof MediaUploadPurpose)[keyof typeof MediaUploadPurpose];
